1d0104d1a3cd874c720b9d31983394e5dc586a34
NHDang95 <2d2l95@gmail.com>
2018-05-04 10:22:15 +0700

    User SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  get 'login', to:'sessions#new'
+  get '/login', to:'sessions#new'
 
-  post 'login', to:'sessions#create'
+  post '/login', to:'sessions#create'
 
-  delete 'logout', to:'sessions#destroy'
+  delete '/logout', to:'sessions#destroy'
 
   root 'static_pages#home'
 
-  get 'help', to:'static_pages#help'
+  get '/help', to:'static_pages#help'
 
-  get 'about', to:'static_pages#about'
+  get '/about', to:'static_pages#about'
 
+  get  '/signup',  to: 'users#new'
+
+  post '/signup',  to: 'users#create'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
