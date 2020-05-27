d3c2bab95bcf817cd955720bf394555676710414
nguyenminhtu <tuunguyen2795@gmail.com>
2020-05-28 00:18:22 +0700

    fix create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,17 +8,20 @@ class CommentsController < ApplicationController
 
   def create
     @comment = current_user.comments.build(comment_params)
+    @micropost = @comment.micropost
+
     if @comment.save
-    flash[:success]="Comment created"
-    redirect_back_or micropost_path(@comment.micropost)
+      flash[:success] = "Comment created"
+      redirect_to @micropost
     else
-    render 'microposts/show'
-  end
+      flash[:danger] = @comment.errors.full_messages.to_s
+      redirect_to @micropost
+    end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:content,:micropost_id,:user_id,:parent_id)
+    params.require(:comment).permit(:content, :micropost_id, :user_id, :parent_id)
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :micropost
   has_many :children, :class_name => "Comment", :foreign_key => "parent_id"
-  belongs_to :parent, :class_name => "Comment", :foreign_key => "parent_id"
+  belongs_to :parent, optional: true, class_name: "Comment", foreign_key: "parent_id"
 end
